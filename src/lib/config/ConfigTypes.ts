2d9628691dfa8a18af7065772201dcf786069f84
ijsKoud <github@ijskoud.dev>
2023-02-18 13:35:17 +0100

    feat: add prisma database handler

--- src/lib/config/ConfigTypes.ts
+++ src/lib/config/ConfigTypes.ts
@@ -5,7 +5,8 @@ export const DEFAULT_RAW_ENV = {
 	INTERNAL_API_KEY: () => randomBytes(32).toString("hex"),
 	ENCRYPTION_KEY: () => randomBytes(32).toString("hex"),
 	DISCORD_CLIENT_ID: "",
-	DISCORD_CLIENT_SECRET: ""
+	DISCORD_CLIENT_SECRET: "",
+	DATABASE_URL: "postgresql://myuser:mypassword@localhost:5432/db?schema=public"
 } as const;
 
 export interface EnvConfig {
