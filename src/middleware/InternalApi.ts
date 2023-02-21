cdf5824c41e1a06168799a508a3cfc1fda4a3bd8
ijsKoud <github@ijskoud.dev>
2023-02-21 21:24:04 +0100

    feat: add auth token middleware

---
+++ src/middleware/InternalApi.ts
@@ -0,0 +1,25 @@
+import type { Request, Response, NextFunction } from "express";
+import { ApplyMiddlewareOptions } from "../lib/Middleware/decorators.js";
+import { Middleware } from "../lib/Middleware/Middleware.js";
+
+@ApplyMiddlewareOptions({
+	name: "internal-api"
+})
+export default class extends Middleware {
+	public override run(req: Request, res: Response, next: NextFunction): void {
+		const { authorization } = req.headers;
+		if (typeof authorization !== "string") {
+			res.status(401).send({ message: "Unauthorized." });
+			return;
+		}
+
+		const [type, token] = authorization.split(/ +/g);
+		console.log(type, token);
+		if (type !== "Bearer" || token !== this.server.config.config.internalApiKey) {
+			res.status(401).send({ message: "Unauthorized." });
+			return;
+		}
+
+		next();
+	}
+}
--- src/routes/auth/csrf.ts
+++ src/routes/auth/csrf.ts
@@ -2,7 +2,8 @@ import type { Request, Response } from "express";
 import { ApiRoute, ApplyOptions } from "../../lib/Api/index.js";
 
 @ApplyOptions({
-	methods: "POST"
+	methods: "POST",
+	middleware: ["internal-api"]
 })
 export default class extends ApiRoute {
 	public override run(req: Request, res: Response) {
