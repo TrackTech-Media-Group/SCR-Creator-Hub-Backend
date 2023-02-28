86882ff8cb0e8e1b01c3db533817448730c6e60b
ijsKoud <github@ijskoud.dev>
2023-02-28 20:37:49 +0100

    feat: add admin state route

---
+++ src/routes/user/admin.ts
@@ -0,0 +1,39 @@
+import type { Request, Response } from "express";
+import { ApiRoute, ApplyOptions } from "../../lib/Api/index.js";
+import jwt from "jsonwebtoken";
+
+@ApplyOptions({
+	methods: "POST"
+})
+export default class extends ApiRoute {
+	private admins = ["304986851310043136"];
+
+	public override async run(req: Request, res: Response) {
+		const { authorization } = req.headers;
+		if (typeof authorization !== "string") {
+			res.send({ token: "" });
+			return;
+		}
+
+		const [type, _token] = authorization.split(/ +/g);
+		if (type !== "User") {
+			res.send({ token: "" });
+			return;
+		}
+
+		const token = jwt.verify(_token, this.server.config.config.encryptionKey);
+		if (typeof token !== "object" || typeof token.session !== "string" || typeof token.userId !== "string") {
+			res.send({ token: "" });
+			return;
+		}
+
+		const session = await this.server.prisma.session.findFirst({ where: { token: token.session, userId: token.userId } });
+		if (!session || session.expirationDate.getTime() <= Date.now() || !this.admins.includes(session?.userId ?? "")) {
+			res.send({ token: "" });
+			return;
+		}
+
+		const csrfToken = this.server.jwt.generateCsrfToken();
+		res.status(200).send(csrfToken);
+	}
+}
