5f7c77dd7e0a55a9201022fb4f12762fde995168
ijsKoud <github@ijskoud.dev>
2023-03-07 16:16:24 +0100

    feat: add search api

---
+++ src/routes/search.ts
@@ -0,0 +1,38 @@
+import type { Request, Response } from "express";
+import Fuse from "fuse.js";
+import { ApiRoute, ApplyOptions } from "../lib/Api/index.js";
+import { chunk } from "../lib/Utils.js";
+
+@ApplyOptions({
+	methods: "GET",
+	middleware: []
+})
+export default class extends ApiRoute {
+	public override async run(req: Request, res: Response) {
+		const { query: _search, page: _page } = req.query;
+		const searchQ = typeof _search === "string" ? _search : "";
+		const page = isNaN(Number(_page)) ? 0 : Number(_page);
+
+		let footage = await this.server.prisma.footage.findMany({ include: { downloads: true } });
+		if (searchQ.length) {
+			const search = new Fuse(footage, {
+				keys: ["name", "useCases", "tagIds"],
+				isCaseSensitive: false
+			});
+			footage = search.search(searchQ).map((sr) => sr.item);
+		}
+
+		const chunks = chunk(footage, 100);
+		const chunkArr = (page > chunks.length ? chunks[chunks.length - 1] : chunks[page]) ?? [];
+
+		res.send({
+			entries: chunkArr.map((f) => ({
+				name: f.name,
+				id: f.id,
+				type: f.type,
+				preview: (f.downloads.find((d) => d.name.startsWith("HD")) ?? f.downloads[0]).url
+			})),
+			pages: chunks.length
+		});
+	}
+}
