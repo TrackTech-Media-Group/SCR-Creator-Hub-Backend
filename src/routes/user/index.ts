09fd912acc7d6be8f8a8f1449cff7ebd08464132
ijsKoud <github@ijskoud.dev>
2023-03-07 14:58:16 +0100

    feat(User): add type to results

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -20,12 +20,14 @@ export default class extends ApiRoute {
 			bookmarks: bookmarks.map((footage) => ({
 				name: footage.name,
 				id: footage.id,
+				type: footage.type,
 				tags: footage.tagIds.map((t) => tags.find((tag) => tag.id === t)?.name).filter(Boolean),
 				preview: footage.downloads.find((d) => d.name.startsWith("HD"))?.url ?? footage.downloads[0].url
 			})),
 			recent: recent.map((footage) => ({
 				name: footage.name,
 				id: footage.id,
+				type: footage.type,
 				preview: footage.downloads.find((d) => d.name.startsWith("HD"))?.url ?? footage.downloads[0].url
 			})),
 			dataRequest: null
