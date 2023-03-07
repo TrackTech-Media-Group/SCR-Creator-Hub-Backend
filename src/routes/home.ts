d03909dfb7265074d94003ae4914ab66e434e3b8
ijsKoud <github@ijskoud.dev>
2023-03-07 16:35:34 +0100

    feat: add home data api

---
+++ src/routes/home.ts
@@ -0,0 +1,28 @@
+import type { Request, Response } from "express";
+import { ApiRoute, ApplyOptions } from "../lib/Api/index.js";
+import _ from "lodash";
+
+@ApplyOptions({
+	methods: "GET",
+	middleware: ["internal-api"]
+})
+export default class extends ApiRoute {
+	public override async run(req: Request, res: Response) {
+		const footage = await this.server.prisma.footage.findMany({ include: { downloads: true } });
+		function* getRandomItem() {
+			for (let i = 0; i < 20; i++) {
+				yield footage[Math.floor(Math.random() * footage.length)];
+			}
+		}
+
+		const randomItems = [...getRandomItem()];
+		res.send(
+			randomItems.map((footage) => ({
+				name: footage.name,
+				id: footage.id,
+				type: footage.type,
+				preview: footage.downloads.find((d) => d.name.startsWith("HD"))?.url ?? footage.downloads[0].url
+			}))
+		);
+	}
+}
