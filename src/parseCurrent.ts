a5d07ec181c7229e2baed6902664158b4dcad97f
ijsKoud <github@ijskoud.dev>
2023-03-25 13:09:31 +0100

    feat: add optimising script

---
+++ src/parseCurrent.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+import axios from "axios";
+import FormData from "form-data";
+import sharp from "sharp";
+
+void (async () => {
+	const prisma = new PrismaClient();
+	await prisma.$connect();
+
+	const footageRaw = await prisma.footage.findMany({ include: { downloads: true } });
+	const footage = footageRaw.filter((f) => !f.preview?.length);
+
+	for await (const item of footage) {
+		console.log(`Running for ${item.name} (${footage.indexOf(item) + 1}/${footage.length})`);
+		const download = item.downloads.find((d) => d.name.includes("HD")) || item.downloads[0];
+
+		try {
+			console.log(`Downloading the image...`);
+			const { data: image } = await axios.get<Buffer>(download.url);
+
+			const transformer = sharp(image, { sequentialRead: true });
+			transformer.rotate();
+
+			switch (download.url.split(".").reverse()[0]) {
+				case "png":
+					transformer.png({ quality: 12 });
+					break;
+				case "jpg":
+				case "jpeg":
+					transformer.jpeg({ quality: 12 });
+					break;
+				case "webp":
+					transformer.webp({ quality: 12 });
+					break;
+			}
+
+			transformer.resize(320, 180);
+			console.log(`Downloaded the image, optimising...`);
+			const optBuffer = await transformer.toBuffer();
+			console.log(`Optimising complete, uploading...`);
+
+			const form = new FormData();
+			form.append("upload", optBuffer);
+
+			const req = await axios<{ url: string }>(`${process.env.UPLOAD_API}/api/upload`, {
+				data: form,
+				method: "POST",
+				headers: { Authorization: process.env.UPLOAD_API_KEY, "Content-Type": "multipart/form-data" }
+			});
+
+			console.log(`Upload completed, editing config...`);
+			await prisma.footage.update({ where: { id: item.id }, data: { preview: req.data.url.replace("http://", "https://") } });
+			console.log(`Continuing with next item`);
+		} catch (err) {
+			console.error(err);
+			console.log(`Failed to optimise ${item.name} (${footage.indexOf(item) + 1}/${footage.length})`);
+		}
+	}
+})();
