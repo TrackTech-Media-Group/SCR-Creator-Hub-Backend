bfba8a2860be3c0001f7a0f45c5a05ccbdb42b66
Daan Klarenbeek <github@ijskoud.dev>
2023-06-05 23:02:14 +0200

    refactor(Server): add updated server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import Server from "./lib/Server.js";
+import "#lib/env.js";
+
+import { CreatorHubServer } from "#lib/Server.js";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 void (async () => {
-	const server = new Server();
+	const server = new CreatorHubServer({ middlewarePath: "", routePath: join(__dirname, "routes") });
 	await server.start();
 })();
--- src/lib/Server.ts
+++ src/lib/Server.ts
@@ -1,67 +1,16 @@
+import { Server } from "@snowcrystals/highway";
+import { ContentManager } from "./ContentManager.js";
 import { PrismaClient } from "@prisma/client";
-import { Logger, LogLevel } from "@snowcrystals/icicle";
-import { bold } from "colorette";
-import express, { type Express } from "express";
-import { ApiHandler } from "./Api/index.js";
-import Config from "./config/index.js";
-import { Jwt } from "./jwt/Jwt.js";
-import { UserManager } from "./User/UserManager.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import { MiddlewareHandler } from "./Middleware/Handler.js";
-import { DataHandler } from "./Data/DataHandler.js";
 
-export default class Server {
-	public server: Express;
-	public logger: Logger;
-	public prisma: PrismaClient;
+export class CreatorHubServer extends Server {
+	/** The manager responsible for all the content on Creator Hub */
+	public readonly contentManager: ContentManager = new ContentManager(this);
 
-	public config: Config;
-	public api: ApiHandler;
-	public middleware: MiddlewareHandler;
-
-	public data: DataHandler;
-
-	public jwt: Jwt;
-	public userManager: UserManager;
-
-	public constructor() {
-		this.server = express();
-		this.logger = new Logger({ level: LogLevel.Debug });
-		this.prisma = new PrismaClient({ log: ["error", "info", "warn"] });
-
-		this.config = new Config(this);
-		this.api = new ApiHandler(this);
-		this.middleware = new MiddlewareHandler(this);
-
-		this.data = new DataHandler(this);
-
-		this.jwt = new Jwt(this);
-		this.userManager = new UserManager(this);
-	}
+	/** The bridge between the application and the PostgreSQL database */
+	public readonly prisma = new PrismaClient();
 
 	public async start() {
-		this.server.use(
-			cors({ credentials: true, origin: ["http://localhost:3000", "https://beta.scrcreate.app", "https://scrcreate.app"] }),
-			cookieParser(),
-			bodyParser.json()
-		);
-
-		await this.config.start();
-		await this.middleware.start();
-		await this.api.start();
-		await this.prisma.$connect();
-
-		await this.data.start();
-
-		this.jwt.load();
-		this.userManager.start();
-
-		this.server.listen(this.config.config.port, this.startup.bind(this));
-	}
-
-	private startup() {
-		this.logger.info(`Server is running on port ${bold(this.config.config.port)}: http://localhost:${this.config.config.port}`);
+		await this.contentManager.load();
+		await this.listen(3000);
 	}
 }
