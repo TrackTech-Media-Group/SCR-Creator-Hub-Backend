271e2cb604bbad6e01bc9c6b6e4a0c76f5e88fc6
Daan Klarenbeek <github@ijskoud.dev>
2023-06-06 11:05:18 +0200

    feat(Server): add error handling

--- src/lib/Server.ts
+++ src/lib/Server.ts
@@ -1,6 +1,11 @@
 import { Server } from "@snowcrystals/highway";
 import { ContentManager } from "./ContentManager.js";
 import { PrismaClient } from "@prisma/client";
+import type { NextFunction, Request, Response } from "express";
+import { ApiError } from "#errors/ApiError.js";
+import { Logger } from "@snowcrystals/icicle";
+import { HttpStatusCode } from "axios";
+import { bold } from "colorette";
 
 export class CreatorHubServer extends Server {
 	/** The manager responsible for all the content on Creator Hub */
@@ -9,8 +14,36 @@ export class CreatorHubServer extends Server {
 	/** The bridge between the application and the PostgreSQL database */
 	public readonly prisma = new PrismaClient();
 
+	/** Private logger instance for error handling */
+	private logger = new Logger({ name: "Error Handler" });
+
 	public async start() {
 		await this.contentManager.load();
-		await this.listen(3000);
+		await this.listen(3000, () => new Logger({ name: "Server" }).info(`Running on port ${bold(3000)}: http://localhost:${3000}`));
+	}
+
+	public override async listen(port: number, cb?: () => void) {
+		await this.middlewareHandler.loadAll(this);
+		await this.routeHandler.loadAll(this);
+
+		this.express.use(this.handleApiError.bind(this));
+		return this.express.listen(port, cb);
+	}
+
+	private handleApiError(error: any, req: Request, res: Response, next: NextFunction) {
+		if (error instanceof ApiError) {
+			if (error.status >= 500) {
+				this.logger.error(`${req.method} ${bold(req.path)} => code: ${bold(error.status)} - `, error.errors);
+				res.status(error.status).send({ "*": "Internal server error, please try again later." });
+
+				return;
+			}
+
+			res.status(error.status).send(error.errors);
+			return;
+		}
+
+		this.logger.error(`${req.method} ${bold(req.path)} => Unknown error while processing API request: `, error);
+		res.status(HttpStatusCode.InternalServerError).send({ "*": "Internal server error, please try again later." });
 	}
 }
