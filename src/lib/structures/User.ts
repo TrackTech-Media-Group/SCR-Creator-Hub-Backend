b930bbc37a6473a17938d6d91b3e85fae6bf554a
Daan Klarenbeek <github@ijskoud.dev>
2023-06-08 13:23:36 +0200

    feat: add User and Session structures

---
+++ src/lib/structures/User.ts
@@ -0,0 +1,43 @@
+import type { User as iUser, Session as iSession } from "@prisma/client";
+import type { Content } from "./Content.js";
+import { Session } from "./Session.js";
+
+export class User implements Omit<iUser, "bookmarks" | "recent"> {
+	/** The unique identifier for this user */
+	public readonly userId: string;
+
+	/** The username of this user */
+	public username: string;
+
+	/** The recently viewed content */
+	public readonly recent: Content[];
+
+	/** The bookmarked content */
+	public readonly bookmarks: Content[];
+
+	/** The date the account was created */
+	public readonly createdAt: Date;
+
+	/** The valid login sessions */
+	public readonly sessions = new Map<string, Session>();
+
+	public constructor(data: iUser & { sessions: iSession[] }, bookmarks: Content[], recent: Content[]) {
+		this.userId = data.userId;
+		this.username = data.username;
+		this.createdAt = data.createdAt;
+		this.recent = recent;
+		this.bookmarks = bookmarks;
+
+		data.sessions.forEach((session) => this.sessions.set(session.token, new Session(session, this)));
+	}
+
+	public toJSON() {
+		return {
+			userId: this.userId,
+			username: this.username,
+			createdAt: this.createdAt,
+			bookmarks: this.bookmarks.map((bookmark) => bookmark.toJSON()),
+			recent: this.recent.map((recent) => recent.toJSON())
+		};
+	}
+}
