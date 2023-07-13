6e917170cc2361ee785937c95ecdf73b9f88c9d5
Daan Klarenbeek <github@ijskoud.dev>
2023-07-13 21:04:11 +0200

    fix(Tags): tags without content not showing

--- src/routes/v1/tags/index.ts
+++ src/routes/v1/tags/index.ts
@@ -4,13 +4,15 @@ import { Utils } from "#lib/utils.js";
 import { ApplyOptions, Route, methods } from "@snowcrystals/highway";
 import type { Request, Response } from "express";
 
+type Filter = ContentTypeFilter | "force";
+
 @ApplyOptions<Route.Options>({ ratelimit: Utils.getRatelimitOptions({ windowMs: 1e3, max: 50 }) })
 export default class extends Route<CreatorHubServer> {
 	public [methods.GET](req: Request, res: Response) {
 		const tags = [...this.server.contentManager.tags.values()];
 		const { type } = this.parseQuery(req.query);
 
-		res.json(tags.filter((tag) => tag.hasType(type)));
+		res.json(type === "force" ? tags : tags.filter((tag) => tag.hasType(type)));
 	}
 
 	/**
@@ -19,7 +21,7 @@ export default class extends Route<CreatorHubServer> {
 	 */
 	private parseQuery(query: Record<string, any>) {
 		const { type: _type } = query;
-		const type: ContentTypeFilter = typeof _type === "string" && CONTENT_TYPE_FILTER.includes(_type as any) ? (_type as any) : "all";
+		const type: Filter = typeof _type === "string" && [...CONTENT_TYPE_FILTER, "force"].includes(_type as any) ? (_type as any) : "all";
 
 		return {
 			type
